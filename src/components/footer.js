c1540b52835cecc0894dca7055819d8aba28286e
0feed <97583655+0feed@users.noreply.github.com>
2022-01-12 19:29:08 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        All rights Reserved, Made with{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="/">ZeroFeed.com</Link>
       </p>
     </div>
   </footer>
